41aa63ac5c9427d6e70950e02ee67344732db06c
Ludovic <ludo.bergeron@icloud.com>
2025-10-13 20:46:29 +0200

    config de base à tester pour tout le monde

---
+++ cap_app/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Demo Flutter',
+      theme: ThemeData(
+        colorSchemeSeed: Colors.indigo,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Hello Flutter')),
+      body: const Center(child: Text('La config est good ouf ')),
+    );
+  }
+}
