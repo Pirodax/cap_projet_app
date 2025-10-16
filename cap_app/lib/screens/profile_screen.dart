0408b4f558bd110298417d5c822dc30dfc9a26e6
petilaire-bell <petilaire-bell@et.esiea.fr>
2025-10-16 15:09:13 +0200

    Première version de la page profile

--- cap_app/lib/screens/profile_screen.dart
+++ cap_app/lib/screens/profile_screen.dart
@@ -1,13 +1,143 @@
 import 'package:flutter/material.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
 
   @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  String? _mutuelle;
+  String? _estEtudiant;
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Profile de l utiliosateur', style: TextStyle(fontSize: 22)),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Page Profile'),
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        foregroundColor: Colors.black,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.person, color: Colors.indigo),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Prénom',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Nom',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+            ),
+            const SizedBox(height: 30),
+            const Text(
+              'Votre Couverture santé:',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+            const Text('mutuel connecté ou non'),
+            const SizedBox(height: 20),
+            DropdownButtonFormField<String>(
+              value: _mutuelle,
+              hint: const Text('Mutuelle'),
+              onChanged: (String? newValue) {
+                setState(() {
+                  _mutuelle = newValue;
+                });
+              },
+              items: <String>['MGEN', 'Alan', 'Axa', 'Mutualia']
+                  .map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Carte vitale:',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 5),
+            const Text('(si non afficher les démarches et conditions)'),
+            const SizedBox(height: 20),
+            const Text(
+              'Situation',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Âge',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+              keyboardType: TextInputType.number,
+            ),
+            const SizedBox(height: 20),
+            DropdownButtonFormField<String>(
+              value: _estEtudiant,
+              hint: const Text('Étudiant ?'),
+              onChanged: (String? newValue) {
+                setState(() {
+                  _estEtudiant = newValue;
+                });
+              },
+              items: <String>['Oui', 'Non']
+                  .map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Département',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              initialValue: '75010',
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+              keyboardType: TextInputType.number,
+            ),
+          ],
+        ),
       ),
     );
   }
